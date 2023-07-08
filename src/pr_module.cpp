d0ab7460f0dfcde78cf1ce349ff82a379849749f
Silverlan <silverlann@gmail.com>
2023-07-08 03:51:57 +0200

    build: fix build errors

--- src/pr_module.cpp
+++ src/pr_module.cpp
@@ -2,50 +2,43 @@
 #include <pragma/lua/luaapi.h>
 #include <pragma/console/conout.h>
 #include <luainterface.hpp>
+#include <mediapipe_pragma_wrapper.h>
 
-extern "C"
+#pragma optimize("", off)
+
+extern "C" {
+// Called after the module has been loaded
+DLLEXPORT bool pragma_attach(std::string &outErr)
 {
-    // Called after the module has been loaded
-    DLLEXPORT bool pragma_attach(std::string &outErr)
-    {
-        // Return true to indicate that the module has been loaded successfully.
-        // If the module could not be loaded properly, return false and populate 'outErr' with a descriptive error message.
-        Con::cout<<"Custom module \"pr_mediapipe\" has been loaded!"<<Con::endl;
-        return true;
-    }
+	// Return true to indicate that the module has been loaded successfully.
+	// If the module could not be loaded properly, return false and populate 'outErr' with a descriptive error message.
+	Con::cout << "Custom module \"pr_mediapipe\" has been loaded!" << Con::endl;
+	//test_mediapipe();
+	return true;
+}
 
-    // Called when the module is about to be unloaded
-    DLLEXPORT void pragma_detach()
-    {
-        Con::cout<<"Custom module \"pr_mediapipe\" is about to be unloaded!"<<Con::endl;
-    }
+// Called when the module is about to be unloaded
+DLLEXPORT void pragma_detach() { Con::cout << "Custom module \"pr_mediapipe\" is about to be unloaded!" << Con::endl; }
 
-    // Lua bindings can be initialized here
-    DLLEXPORT void pragma_initialize_lua(Lua::Interface &lua)
-    {
-        auto &libDemo = lua.RegisterLibrary("pr_mediapipe");
-        libDemo[luabind::def("print",+[]() {
-            Con::cout<<"Hello World"<<Con::endl;
-        })];
+// Lua bindings can be initialized here
+DLLEXPORT void pragma_initialize_lua(Lua::Interface &lua)
+{
+	auto &libDemo = lua.RegisterLibrary("mediapipe");
+	libDemo[luabind::def(
+	  "print", +[]() { Con::cout << "Hello World" << Con::endl; })];
+	//libDemo[luabind::def("test", &test_mediapipe)];
 
-        struct DemoClass
-        {
-            DemoClass() {}
-            void PrintWarning(const std::string &msg)
-            {
-                Con::cwar<<msg<<Con::endl;
-            }
-        };
+	struct DemoClass {
+		DemoClass() {}
+		void PrintWarning(const std::string &msg) { Con::cwar << msg << Con::endl; }
+	};
 
-        auto classDef = luabind::class_<DemoClass>("DemoClass");
-        classDef.def(luabind::constructor<>());
-        classDef.def("PrintWarning",&DemoClass::PrintWarning);
-        libDemo[classDef];
-    }
+	auto classDef = luabind::class_<DemoClass>("DemoClass");
+	classDef.def(luabind::constructor<>());
+	classDef.def("PrintWarning", &DemoClass::PrintWarning);
+	libDemo[classDef];
+}
 
-    // Called when the Lua state is about to be closed.
-    DLLEXPORT void pragma_terminate_lua(Lua::Interface &lua)
-    {
-        
-    }
+// Called when the Lua state is about to be closed.
+DLLEXPORT void pragma_terminate_lua(Lua::Interface &lua) {}
 };
